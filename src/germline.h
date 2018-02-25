faa37ddae9ece8882b51d907ba8482011756fc0e
jfong <jfong361@gmail.com>
2018-02-26 00:11:51 +1100

    Add string methods to Germline class
    Add size, subscript, substr and trim methods to easily conduct
    recombination operations on germline object itself.

--- src/germline.h
+++ src/germline.h
@@ -16,10 +16,14 @@
 namespace immulator {
 
 class Germline {
-friend std::ostream &operator<<(std::ostream &os, const immulator::Germline &germ);
-friend Germline operator+(Germline lhs, const Germline& rhs);
+    friend std::ostream &operator<<(std::ostream &os, const immulator::Germline &germ);
+
+    friend Germline operator+(Germline lhs, const Germline &rhs);
+
 public:
-    Germline(const std::string& name, const std::string &ascnum, const std::string &seq) :
+    using size_type = std::string::size_type;
+
+    Germline(const std::string &name, const std::string &ascnum, const std::string &seq) :
             name_(name), ascnum_(ascnum), seq_(seq) {}
 
     Germline &operator+=(const Germline &other) {
@@ -28,6 +32,11 @@ public:
         seq_ += other.seq_;
         return *this;
     }
+
+    char &operator[](size_type index) { return seq_[index]; }
+
+    const char &operator[](size_type index) const { return seq_[index]; }
+
 public:
 
     const std::string family_name() const {
@@ -35,18 +44,19 @@ public:
             return name_.substr(0, name_.find_first_of('-'));
         } else {
             auto tokens = immulator::split_string(name_, std::string(1, recombination_delim));
-            std::transform(tokens.begin(), tokens.end(), tokens.begin(), [] (auto &tok) {
+            std::transform(tokens.begin(), tokens.end(), tokens.begin(), [](auto &tok) {
                 return tok.substr(0, tok.find_first_of('-'));
             });
             return immulator::join_string(tokens.cbegin(), tokens.cend(), std::string(1, recombination_delim));
         }
     }
+
     const std::string gene_name() const {
         if (name_.find(recombination_delim) == std::string::npos) {
             return name_.substr(0, name_.find_first_of('*'));
         } else {
             auto tokens = immulator::split_string(name_, std::string(1, recombination_delim));
-            std::transform(tokens.begin(), tokens.end(), tokens.begin(), [] (auto &tok) {
+            std::transform(tokens.begin(), tokens.end(), tokens.begin(), [](auto &tok) {
                 return tok.substr(0, tok.find_first_of('*'));
             });
             return immulator::join_string(tokens.cbegin(), tokens.cend(), std::string(1, recombination_delim));
@@ -56,6 +66,24 @@ public:
     const std::string name() const {
         return name_;
     }
+
+    /// Returns a sub-sequence after cutting at start, ending at ncount bases later (i.e. exactly the same as
+    /// regular std::string::substr)
+    /// \param start start index to perform substr
+    /// \param ncount end at count bases after start index
+    /// \return substring (sub-sequence)
+    std::string substr(size_type start = 0, size_type ncount = std::string::npos) const {
+        return seq_.substr(start, ncount);
+    }
+
+    /// Performs an in-place substr
+    /// \param start start index to perform trimmming
+    /// \param ncount trim ncount bases starting from starting index
+    void trim(size_type start = 0, size_type ncount = std::string::npos) {
+        seq_ = seq_.substr(start, ncount);
+    }
+
+    size_type size() const { return seq_.size(); }
 
 private:
     std::string seq_;
@@ -67,11 +95,10 @@ private:
 std::ostream
 &operator<<(std::ostream &os, const immulator::Germline &germ);
 
-immulator::Germline operator+(Germline lhs, const Germline& rhs);
+immulator::Germline operator+(Germline lhs, const Germline &rhs);
 
 
 }
-
 
 
 #endif //IMMULATOR_GERMLINE_H
