3f468ad0a4b9fbbac3362c3baeb08f7136f8d7b3
jfong <jfong361@gmail.com>
2018-03-03 23:12:34 +1100

    Add optional class to immulator namespace
    Emulate C++17's std::optional

--- src/immutils.h
+++ src/immutils.h
@@ -9,10 +9,15 @@
 
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <cctype>
 
 namespace immulator {
 inline std::string strip_string(const std::string &str, const std::string &delim);
 inline std::vector<std::string> split_string(const std::string &str, const std::string &delim);
+inline std::string translate(const std::string &ntseq);
+inline std::string &toupper(std::string &str);
+inline std::string toupper(const std::string &str);
 template<typename In> inline std::string join_string(const In &begin, const In &end, const std::string &delim);
 
 std::string
@@ -42,6 +47,25 @@ split_string(const std::string &str, const std::string &delim) {
     return collector;
 }
 
+/// warning: when using this function, it's doing touooer INPLACE, then returning the parameter itself
+/// \param str string
+/// \return str, convert to uppercase inplace
+std::string
+&toupper(std::string &str) {
+    std::transform(str.begin(), str.end(), str.begin(), [] (char c) { return std::toupper(c); });
+    return str;
+}
+
+std::string
+toupper(const std::string &str) {
+    return std::accumulate(str.cbegin(),
+                           str.cend(),
+                           std::string(""),
+                           [] (const std::string &acc,  char c) -> std::string {
+                               return acc + std::string(1, std::toupper(c));
+                           });
+}
+
 template<typename In>
 std::string
 join_string(const In &begin, const In &end, const std::string &delim) {
@@ -50,6 +74,90 @@ join_string(const In &begin, const In &end, const std::string &delim) {
     });
 }
 
+std::string
+translate(const std::string &ntseq) {
+    static constexpr char STOP_CODON = '*';
+    static std::unordered_map<std::string, char> codon_table = {
+            {"TTT", 'F'}, {"TCT", 'S'}, {"TAT", 'Y'}, {"TGT", 'C'},
+            {"TTC", 'F'}, {"TCC", 'S'}, {"TAC", 'Y'}, {"TGC", 'C'},
+            {"TTA", 'L'}, {"TCA", 'S'}, {"TAA", STOP_CODON}, {"TGA", STOP_CODON},
+            {"TTG", 'L'}, {"TCG", 'S'}, {"TAG", STOP_CODON}, {"TGG", 'W'},
+            {"CTT", 'L'}, {"CCT", 'P'}, {"CAT", 'H'}, {"CGT", 'R'},
+            {"CTC", 'L'}, {"CCC", 'P'}, {"CAC", 'H'}, {"CGC", 'R'},
+            {"CTA", 'L'}, {"CCA", 'P'}, {"CAA", 'Q'}, {"CGA", 'R'},
+            {"CTG", 'L'}, {"CCG", 'P'}, {"CAG", 'Q'}, {"CGG", 'R'},
+            {"ATT", 'I'}, {"ACT", 'T'}, {"AAT", 'N'}, {"AGT", 'S'},
+            {"ATC", 'I'}, {"ACC", 'T'}, {"AAC", 'N'}, {"AGC", 'S'},
+            {"ATA", 'I'}, {"ACA", 'T'}, {"AAA", 'K'}, {"AGA", 'R'},
+            {"ATG", 'M'}, {"ACG", 'T'}, {"AAG", 'K'}, {"AGG", 'R'},
+            {"GTT", 'V'}, {"GCT", 'A'}, {"GAT", 'D'}, {"GGT", 'G'},
+            {"GTC" ,'V'}, {"GCC", 'A'}, {"GAC", 'D'}, {"GGC", 'G'},
+            {"GTA", 'V'}, {"GCA", 'A'}, {"GAA", 'E'}, {"GGA", 'G'},
+            {"GTG", 'V'}, {"GCG", 'A'}, {"GAG", 'E'}, {"GGG", 'G'}
+    };
+    std::string nt(ntseq);
+
+    std::string aa;
+    for (std::string::size_type i = 0; i < nt.size() / 3; ++i) {
+        auto codon = immulator::toupper(nt.substr(i * 3, 3));
+        if (codon_table.find(codon) == codon_table.end()) {
+            std::cerr << "WARNING: Unknown codon encountered: " << codon << '\n';
+        } else {
+            aa += codon_table[codon];
+        }
+    }
+    return aa;
+}
+
+template<typename T>
+class optional {
+public:
+    optional() = default;
+    optional(const T& item) : item_(item), b_(true) {}
+
+    const T &operator*() const {
+        return item_;
+    }
+
+    const T *operator->() const {
+        return &item_;
+    }
+
+    T &operator*() {
+        return item_;
+    }
+
+    T *operator->() {
+        return &item_;
+    }
+
+    operator bool() const {
+        return this->has_value();
+    }
+
+    bool has_value() const {
+        return b_;
+    }
+
+    T value() const {
+        return **this;
+    }
+
+    T value_or(const T& other) const {
+        if (this->has_value()) {
+            return **this;
+        } else {
+            return other;
+        }
+    }
+
+private:
+    T item_;
+    bool b_ = false;
+};
+
 }
 
 #endif //IMMULATOR_IMMUTILS_H
+
+
